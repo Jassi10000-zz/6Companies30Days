b9267d4c868f53327a198538039a25798049ca09
Jaskeerat Singh <jaskeerat10000@gmail.com>
2022-01-08 12:56:56 +0530

    added bracketsInMCM, maxOfSubarrOfSizeK , PhoneDirectory

---
+++ 2-Amazon/MaxOfSubarrOfSizeK.java
@@ -0,0 +1,30 @@
+class MaxOfSubarrOfSize {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        ArrayList<Integer> res = new ArrayList<Integer>(0);
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (i = 0; i < k; i++) {
+            while (dq.isEmpty() == false && arr[i] >= arr[dq.peekLast()])
+                dq.pollLast();
+            dq.add(i);
+        }
+
+        for (; i < n; i++) {
+            res.add(arr[dq.peek()]);
+            while (dq.isEmpty() == false && (dq.peekFirst() <= i - k))
+                dq.pollFirst();
+
+            while (dq.isEmpty() == false && (arr[i] >= arr[dq.peekLast()]))
+                dq.pollLast();
+
+            dq.add(i);
+        }
+        res.add(arr[dq.peek()]);
+        dq.pollFirst();
+
+        return res;
+
+    }
+}
