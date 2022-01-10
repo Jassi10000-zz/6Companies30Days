3b53354aeebe7a0f7a9b1589793478eab7013ec9
Jaskeerat Singh <jaskeerat10000@gmail.com>
2022-01-10 17:16:51 +0530

    added countWayToNthStair and FirstNonRepeatingCharInStream

---
+++ 2-Amazon/CountWaytoNthStair.java
@@ -0,0 +1,4 @@
+class CountWaytoNthStair{
+	Long countWays(int m){
+       return (long)(m/2)+1;
+}
---
+++ 2-Amazon/FirstNonRepeatingCharInStream.java
@@ -0,0 +1,20 @@
+class FirstNonRepeatingCharInAStream{
+	public String FirstNonRepeating(String A)
+    {
+        StringBuilder sb=new StringBuilder();
+        int isRepet[]=new int[26];
+        Deque<Character> queue=new ArrayDeque<>();
+        for(int i=0;i<A.length();i++){
+            char ch=A.charAt(i);
+            if(isRepet[ch-'a']==0)
+               queue.add(ch);
+            
+            isRepet[ch-'a']++;
+            while(!queue.isEmpty() && isRepet[queue.peek()-'a']!=1)
+               queue.poll();
+            sb.append(queue.isEmpty() ? '#' : queue.peek());
+        }
+        return sb.toString();
+        
+    }
+}
