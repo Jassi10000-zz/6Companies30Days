e62b7749720d9bc0f739c06e835cdc121d9d70e8
Jaskeerat Singh <jaskeerat10000@gmail.com>
2022-01-30 17:57:51 +0530

    added divideTwoIntegers and largestNoInSwaps

---
+++ 6-Walmart/largestNoInKSwaps.java
@@ -0,0 +1,42 @@
+class Res {
+    static String max = "";
+}
+
+class largestNoInKSwaps
+{
+    public static void findMaximumNumUtil(char ar[], int k, Res r)
+    {
+        if (k == 0) return;
+        int n = ar.length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (ar[j] > ar[i])
+                {
+                    char temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+
+                    String st = new String(ar);
+                    if (r.max.compareTo(st) < 0)
+                    {
+                        r.max = st;
+                    }
+                    findMaximumNumUtil(ar, k - 1, r);
+                    temp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = temp;
+                }
+            }
+        }
+    }
+    public static String findMaximumNum(String str, int k)
+    {
+        Res r = new Res();
+        r.max = str;
+        findMaximumNumUtil(str.toCharArray(), k, r);
+        return r.max;
+    }
+
+}
